ab61cd8d82130451469bd41a4c39ed980a5b8adc
Binary0-1 <prasanmishra330@gmail.com>
2025-10-02 23:28:11 +0530

    Water Bottles 2

---
+++ lc3100.java
@@ -0,0 +1,12 @@
+public class lc3100 {
+
+    public int maxBottlesDrunk(int numBottles, int numExchange) {
+        int ans = numBottles;
+        for (int empty = numBottles; empty >= numExchange; numExchange++) {
+            ans++;
+            empty -= numExchange - 1;
+        }
+        return ans;
+    }
+
+}
