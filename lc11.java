b9cf0c5ec3fea0812df2bb245de086d7c00b6c6a
Binary0-1 <prasanmishra330@gmail.com>
2025-10-04 09:14:21 +0530

    Container with most water

---
+++ lc11.java
@@ -0,0 +1,24 @@
+
+class lc11 {
+    public static int maxArea(int[] height) {
+
+        int left = 0;
+        int right = height.length - 1;
+        int maxArea = 0;
+
+        while (left < right) {
+            maxArea = Math.max(maxArea, (Math.min(height[left], height[right]) * (right - left)));
+            if (height[left] < height[right]) {
+                left++;
+            } else {
+                right--;
+            }
+        }
+        return maxArea;
+    }
+
+    public static void main(String[] args) {
+        int[] nums = { 1, 8, 6, 2, 5, 4, 8, 3, 7 };
+        System.out.println(maxArea(nums));
+    }
+}
