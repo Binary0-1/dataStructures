7b2015150f5fd2aed5bccf7e49862c05a3343453
Binary0-1 <prasanmishra330@gmail.com>
2025-11-25 22:15:07 +0530

    smallest integer divisible by k

--- trees/Tree.java
+++ trees/Tree.java
@@ -1,6 +1,5 @@
 package trees;
-
-import java.util.Stack;
+import java.util.*;
 
 public class Tree {
 
@@ -27,14 +26,24 @@ public class Tree {
         }
     }
 
+
+    static void bfs(Node root){
+        Queue<Node> q = new LinkedList<>();
+        q.add(root);
+
+        while(!q.isEmpty()){
+            Node curr = q.poll();
+            System.out.println(curr);
+            if(curr.left != null) q.add(curr.left);
+            if(curr.right != null) q.add(curr.right);
+        }
+    }
+
     public static void main(String[] args) {
         Node root = new Node(1);
         root.left = new Node(2);
-        root.right = new Node(3);
-
-        inorder(root);
-
-        inorderIterative(root);
+        root.right = new Node(4);
+        bfs(root);
     }
     
 }
