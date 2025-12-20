d7ec64998858a43d088e3526d730a0aa8d5dfff1
Binary0-1 <prasanmishra330@gmail.com>
2025-12-20 22:53:06 +0530

    House Robber

---
+++ dp/HouseRobber.java
@@ -0,0 +1,55 @@
+package dp;
+
+// You are a professional robber planning to rob houses along a street. Each house has a certain amount of money stashed, the only constraint stopping you from robbing each of them is that adjacent houses have security systems connected and it will automatically contact the police if two adjacent houses were broken into on the same night.
+
+import java.util.Arrays;
+
+
+// Given an integer array nums representing the amount of money of each house, return the maximum amount of money you can rob tonight without alerting the police.
+
+// Example 1:
+
+// Input: nums = [1,2,3,1]
+// Output: 4
+// Explanation: Rob house 1 (money = 1) and then rob house 3 (money = 3).
+// Total amount you can rob = 1 + 3 = 4.
+// Example 2:
+
+// Input: nums = [2,7,9,3,1]
+// Output: 12
+// Explanation: Rob house 1 (money = 2), rob house 3 (money = 9) and rob house 5 (money = 1).
+// Total amount you can rob = 2 + 9 + 1 = 12.
+
+public class HouseRobber {
+
+    static int helper(int n, int[] nums, int[] dp){
+
+        if(n == 0) return nums[n];
+        if(n < 0) return 0;
+
+        if(dp[n] != -1){
+            return dp[n];
+        }
+
+        int left ;
+        int right ;
+
+        left = nums[n] + helper(n-2,nums,dp);
+        right = helper(n-1, nums,dp);
+        dp[n] =  Math.max(left,right);
+
+        return dp[n];
+    }
+
+    static int rob(int[] nums) {
+        int[] dp = new int[nums.length + 1];
+        Arrays.fill(dp, -1);
+       return helper(nums.length-1, nums, dp);
+    }
+
+    public static void main(String[] args) {
+        int[] arr = {2,7,9,3,1};
+        System.out.println(rob(arr));
+    }
+
+}
